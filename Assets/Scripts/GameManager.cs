42b41f7ab50826dd1fd3f4a225cb3bed4ec28b3a
felipe0703 <felipe.aguilera.g@gmail.com>
2019-08-18 21:31:06 -0400

    proyecto final

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public int totalEnemigos;
+    public int intentosRestantes;
+    public Vector3 posicionInicialJugador;
+    public List<GameObject> jugadores;
+    private int jugadorActual = 0;
+    private bool pausado = false;
+    public GameObject activable;
+    public Transform gridIntentos;
+    public GameObject intentoIcon;
+    public Transform macrofago;
+
+    #region Pantallas
+    public GameObject pantallaVictoria;
+    public GameObject pantallaDerrota;
+    #endregion
+
+    
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        intentosRestantes = jugadores.Count;
+        posicionInicialJugador = jugadores[jugadorActual].transform.position + macrofago.position;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LlenarVidas(0.5f));
+    }
+
+    IEnumerator LlenarVidas(float time)
+    {
+        int cont = 0;
+        do
+        {
+            yield return new WaitForSeconds(time);
+            GameObject icon = intentoIcon;
+            icon.GetComponent<Image>().sprite = jugadores[cont].transform.GetChild(0).GetComponent<SpriteRenderer>().sprite;
+            Instantiate(intentoIcon, gridIntentos);
+            cont++;
+        } while (cont < jugadores.Count);
+        yield return new WaitForSeconds(time);
+        activable.SetActive(true);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void IntentoMenos()
+    {
+        if(!pausado && intentosRestantes <= 0 && totalEnemigos > 0)
+        {
+            GameOver(false);
+        }
+        else if(!pausado)
+        {
+            jugadorActual++;
+            Instantiate(jugadores[jugadorActual], posicionInicialJugador, jugadores[jugadorActual].transform.rotation,macrofago);  
+        }
+    }
+
+    public void Lanzado()
+    {
+        Destroy(gridIntentos.GetChild(0).gameObject);
+        intentosRestantes--;
+    }
+
+    public void EnemigoMuerto()
+    {
+        if(totalEnemigos-1 <= 0)
+        {
+            GameOver(true);
+        }
+        else
+        {
+            totalEnemigos--;
+        }
+    }
+
+    private void GameOver(bool win)
+    {
+        pausado = true;
+        if(win)
+        {
+            pantallaVictoria.SetActive(true);
+        }
+        else
+        {
+            pantallaDerrota.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Projectil.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectil : MonoBehaviour
+{
+    public float maxStretch = 3.0f;
+
+    private Vector2 prevVel;
+
+    private float maxStretchSqr;
+    public LineRenderer cFront;
+    public LineRenderer cBack;
+
+    private Transform catapult;
+
+    private Ray rayToMouse;
+    private Ray leftCatapultToProjectile;
+
+    private SpringJoint2D spring;
+    private Rigidbody2D rb;
+
+    bool clickedOn;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        cFront = GameObject.Find("Mano_i").GetComponent<LineRenderer>();
+        cBack = GameObject.Find("Mano_d").GetComponent<LineRenderer>();
+
+        spring = GetComponent<SpringJoint2D>();
+
+        spring.connectedBody = GameObject.Find("Punto_anclaje").GetComponent<Rigidbody2D>();
+
+        catapult = spring.connectedBody.transform;
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        
+
+        rayToMouse = new Ray(catapult.position, Vector3.zero);
+        leftCatapultToProjectile = new Ray(cFront.transform.position, Vector3.zero);
+
+        maxStretchSqr = maxStretch * maxStretch;
+
+        LineRendererSetup();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spring != null && Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            spring.enabled = false;
+            clickedOn = true;
+        }
+        else if (spring != null && Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            spring.enabled = true;
+            rb.isKinematic = false;
+            clickedOn = false;
+        }
+        if (spring != null && clickedOn)
+        {
+            Dragging();
+        }
+
+        if(spring != null)
+        {
+            if(!rb.isKinematic && prevVel.sqrMagnitude > rb.velocity.sqrMagnitude)
+            {
+                Destroy(spring);
+                rb.velocity = prevVel;
+                GameManager.Instance.Lanzado();
+            }
+            if(!clickedOn)
+            {
+                prevVel = rb.velocity;
+            }
+            LineRendererUpdate();
+        }
+        else
+        {
+            cFront.enabled = false;
+            cBack.enabled = false;
+        }
+    }
+
+    void LineRendererSetup()
+    {
+        cFront.enabled = true;
+        cBack.enabled = true;
+
+        cFront.SetPosition(0, cFront.transform.position);
+        cBack.SetPosition(0, cBack.transform.position);
+        cFront.SetPosition(1, transform.position);
+        cBack.SetPosition(1, transform.position);
+
+        cFront.sortingLayerName = "Player";
+        cBack.sortingLayerName = "Player";
+
+        cFront.sortingOrder = 3;
+        cBack.sortingOrder = 1;
+    }
+
+    void LineRendererUpdate()
+    {
+        Vector2 catapultToProjectile = transform.position - cFront.transform.position;
+        leftCatapultToProjectile.direction = catapultToProjectile;
+        Vector3 holdPoint = leftCatapultToProjectile.GetPoint(catapultToProjectile.magnitude);
+        cFront.SetPosition(1, holdPoint);
+        cBack.SetPosition(1, holdPoint);
+    }
+
+    void Dragging()
+    {
+        Vector3 mouseWorldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 catapulToMouse = mouseWorldPoint - catapult.position;
+
+        if(catapulToMouse.sqrMagnitude > maxStretchSqr)
+        {
+            rayToMouse.direction = catapulToMouse;
+            mouseWorldPoint = rayToMouse.GetPoint(maxStretch);
+        }
+
+        mouseWorldPoint.z = 0f;
+
+        transform.position = mouseWorldPoint;
+    }
+}
