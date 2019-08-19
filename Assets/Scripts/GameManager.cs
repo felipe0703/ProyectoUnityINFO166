e3ab53636dd8351bc61c2df09c3a34b2bad3ccd9
felipe0703 <felipe.aguilera.g@gmail.com>
2019-08-18 23:40:34 -0400

    funcionalidad del timer

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -22,7 +23,12 @@ public class GameManager : MonoBehaviour
     public GameObject pantallaDerrota;
     #endregion
 
-    
+    [Tooltip("Tiempo inicial en segundo")] public int tiempoInicial;
+    [Tooltip("Escala del tiempo del Reloj")] [Range(-10f, 10f)] public float escalaDeTiempo = 1;
+    private float tiempoDelFrameConTimeScale = 0f;
+    private float tiempoAMostrarEnSegundos = 0f;
+    private float escalaDeTiempoAlPausar, escalaDeTiempoInicial;
+    public TextMeshProUGUI textTimer;
 
     private void Awake()
     {
@@ -31,11 +37,17 @@ public class GameManager : MonoBehaviour
             Instance = this;
         }
         intentosRestantes = jugadores.Count;
-        posicionInicialJugador = jugadores[jugadorActual].transform.position + macrofago.position;
+        posicionInicialJugador = jugadores[jugadorActual].transform.position + macrofago.position;      
     }
+
     // Start is called before the first frame update
     void Start()
     {
+        //  TIMER
+        escalaDeTiempoInicial = escalaDeTiempo;         //  Establecer la escala de tiempo original      
+        tiempoAMostrarEnSegundos = tiempoInicial;       //  Inicializamos la variables que acumular
+        ActualizarReloj(tiempoInicial);
+        pausado = true;
         StartCoroutine(LlenarVidas(0.5f));
     }
 
@@ -52,11 +64,20 @@ public class GameManager : MonoBehaviour
         } while (cont < jugadores.Count);
         yield return new WaitForSeconds(time);
         activable.SetActive(true);
+        pausado = false;
     }
     // Update is called once per frame
     void Update()
     {
-        
+        if (!pausado)
+        {
+            //  La siguiente variable representa el tiempo de cada frame considerando la escala de tiempo
+            tiempoDelFrameConTimeScale = Time.deltaTime * escalaDeTiempo;
+
+            //  La siguiente variable va acumulando el tiempo transcurrido para luego mostrarlo en el reloj
+            tiempoAMostrarEnSegundos += tiempoDelFrameConTimeScale;
+            ActualizarReloj(tiempoAMostrarEnSegundos);
+        }
     }
 
     public void IntentoMenos()
@@ -101,5 +122,75 @@ public class GameManager : MonoBehaviour
         {
             pantallaDerrota.SetActive(true);
         }
+    }
+
+    //  TIMER
+    public void ActualizarReloj(float tiempoEnSegundos)
+    {
+        int minutos = 0;
+        int segundos = 0;
+        string textoDelReloj;
+
+        //  Asegurar que el tiempo no sea negativo
+        if (tiempoEnSegundos < 0)
+        {
+            tiempoEnSegundos = 0;
+            GameOver(false);
+        }
+
+        //  Calcular minutos y segundos
+        minutos = (int)tiempoEnSegundos / 60;
+        segundos = (int)tiempoEnSegundos % 60;
+
+
+        // Formato con el que se ve el tiempo
+        //  Crear la cadena de caracteres con 2 dígitos para los minutos y segundos, separados por  ":"
+        if (minutos >= 10)
+        {
+            textoDelReloj = minutos.ToString("00") + ":" + segundos.ToString("00");
+        }
+        else if (minutos >= 1)
+        {
+            textoDelReloj = minutos.ToString("0") + ":" + segundos.ToString("00");
+        }
+        else if (segundos < 10)
+        {
+            textoDelReloj = segundos.ToString("0");
+        }
+        else
+        {
+            textoDelReloj = segundos.ToString("00");
+        }
+
+
+        //  Actualizar el elemento de text de UI con la cadena de caracteres
+        textTimer.text = textoDelReloj;
+    }
+
+    public void Pausar()
+    {
+        if (!pausado)
+        {
+            pausado = true;
+            escalaDeTiempoAlPausar = escalaDeTiempo;
+            escalaDeTiempo = 0;
+        }
+    }
+
+    public void Continuar()
+    {
+        if (pausado)
+        {
+            pausado = false;
+            escalaDeTiempo = escalaDeTiempoAlPausar;
+        }
+    }
+
+    public void Reiniciar()
+    {
+        pausado = false;
+        escalaDeTiempo = escalaDeTiempoInicial;
+        tiempoAMostrarEnSegundos = tiempoInicial;
+        ActualizarReloj(tiempoAMostrarEnSegundos);
     }
 }
